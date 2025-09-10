90cb885dd1d26ad7c9b1a874a6e606e5723a2775
MananTheCoder <manangoyaljee@gmail.com>
2025-09-10 13:13:15 +0530

    Completed Constructors, Class-Object Creation, Access Specifiers

--- 3_Sem/A1/OOPS/basic_class_object.cpp
+++ 3_Sem/A1/OOPS/basic_class_object.cpp
@@ -8,16 +8,20 @@ public:
     string company;
     string color;
     string model;
+    string body_material;
     bool transmission;
     int seats;
     double top_speed;
     char fuel;
     double mileage;
 
-    // member functions
-    void calculate_mileage(int km, int litres)
+    Cars()
     {
-        mileage = (double)km / litres;
+        this->company = "";
+        this->color = "";
+        this->model = "";
+        // this->seats = NULL;
+        //
     }
 
     Cars(string Color, string company, int seats, string model)
@@ -28,25 +32,56 @@ public:
         this->model = model;
     }
 
-    void drive()
+    Cars(Cars *obj)
     {
-        cout << "Hi, the car is moving.\n";
+        this->company = obj->company;
+    }
+
+    Cars(Cars &obj)
+    {
+        this->company = obj.company;
+        this->model = obj.model;
+        this->color = obj.color;
+        this->seats = obj.seats;
+    }
+
+    // member functions
+
+    void drive(string driver)
+    {
+        cout << driver << " is driving " << this->company << "'s " << this->model << ".\n";
     }
 
     void shiftGear(int gear)
     {
         cout << "The new gear is " << gear << ".\n";
     }
+
+private:
+    void calculate_mileage(int km, int litres)
+    {
+        mileage = (double)km / litres;
+    }
 };
 
 int main()
 {
     // Cars *keshvi_car = new Cars();
-    // Cars keshvi_car;
-    // keshvi_car.color = "Black";
-    // keshvi_car.company = "BMW";
-    // keshvi_car.seats = 5;
+    Cars keshvi_car;
+    keshvi_car.color = "Black";
+    keshvi_car.company = "BMW";
+    keshvi_car.seats = 5;
 
     Cars rudra_car("silver", "toyota", 7, "innova"); //
-    cout << rudra_car.company << "\n";
+    // cout << rudra_car.company << "\n";
+
+    // Cars *Prem_car = new Cars(keshvi_car);
+    Cars Prem_car(keshvi_car);
+    // cout << Prem_car.company << "\n";
+    Prem_car.seats = 2;
+    Prem_car.model = "M4 Competition";
+    Prem_car.body_material = "Carbon";
+    Prem_car.color = "Matt Black";
+
+    Prem_car.drive("Prem");
 }
